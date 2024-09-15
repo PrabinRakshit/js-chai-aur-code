19691acd83cdbd3ab59276c12984506fee1a6b51
PrabinRakshit <79787785+PrabinRakshit@users.noreply.github.com>
2024-09-15 18:23:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+let score = "35asd"
+let score2 = null
+let score3 = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log(typeof score2);
+// console.log(typeof (score2));
+
+let valueInNumber2 = Number(score2)
+
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+// console.log(typeof score3);
+// console.log(typeof (score3));
+
+let valueInNumber3 = Number(score3)
+
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "prabin" => true
+
+let someNum = 34
+let stringNum = String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
