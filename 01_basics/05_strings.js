e0cc49c9bc49ff4767f827e3c60fa60006b5c560
PrabinRakshit <79787785+PrabinRakshit@users.noreply.github.com>
2024-10-02 18:01:24 +0000

    strings topic

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "Prabin"
+const repoCount = 50
+
+// Old way of writing dynamic values
+// console.log(name + repoCount + " value");
+
+// String interpolation method
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('PrabinRakshit');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); // Prototype methods/functions
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(10));
+console.log(gameName.indexOf('s'));
