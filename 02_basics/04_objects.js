71318e9e4b2000ce2bd945dd2a59e09dbdf09bb8
PrabinRakshit <79787785+PrabinRakshit@users.noreply.github.com>
2024-10-21 17:24:51 +0000

    further objects topics

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,5 +22,31 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
 // const obj3 = {obj1, obj2};
-const obj3 = Object.assign({}, obj1, obj2) //syntax : Object.assign(target, source) -> stores all the recurring objects to target
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2) //syntax : Object.assign(target, source) -> stores all the recurring objects to target
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "p@gmail.com"
+    },
+    {
+        id: 2,
+        email: "q@gmail.com"
+    },
+    {
+        id: 3,
+        email: "r@gmail.com"
+    }
+]
+
+// console.log(users[0].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIns'));
