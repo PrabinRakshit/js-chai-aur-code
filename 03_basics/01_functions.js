2d907f8493c9f9a18b5fdf05eb1a22ea6384c752
PrabinRakshit <79787785+PrabinRakshit@users.noreply.github.com>
2024-10-28 18:14:59 +0000

    functions and scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,4 +7,41 @@ function loginUserMessage(username = "Sam"){ //Default value if param is empty
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num){ //rest operator
+    return num;
+}
+
+function calculateCartPrice2(val1, val2, ...num){ //rest operator
+    return num;
+}
+
+// console.log(calculateCartPrice(100, 200, 300, 400, 500));
+// console.log(calculateCartPrice2(100, 200, 300, 400, 500));
+
+const user = {
+    username: "Prabin",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user);
+
+handleObject({
+    username: "Broom",
+    price: 399
+})
+
+const myNewArray = [200, 300, 400, 500, 100]
+
+function returnSecondValue(arr){
+    return arr[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 300, 100, 500]));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+if(true){
+    let num = 10;
+    const bal = 20;
+    var count = 30;
+}
+
+// console.log(num);
+// console.log(bal);
+// console.log(count);
+
+// for(let i = 0; i<10; i++){
+//     const element = array[i]
+// }
