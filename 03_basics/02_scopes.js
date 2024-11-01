7cb688e545b0401bb8eb01059dfee0f3af2975f9
PrabinRakshit <79787785+PrabinRakshit@users.noreply.github.com>
2024-11-01 12:40:09 +0000

    function concepts and hoisting in function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,35 @@ if(true){
 // for(let i = 0; i<10; i++){
 //     const element = array[i]
 // }
+
+
+function one(){
+    const username = "Prabin"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+
+one();
+
+
+// +++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++++++
+
+// console.log(addOne(5)) // Works because of hoisting
+function addOne(num){ // Normal function
+    return num + 1;
+}
+
+// console.log(addTwo(5)) // Doesn't work because of declaration and assigned to a variable that is initialized later
+const addTwo = function(num){ //Function Expression
+    return num + 2;
+}
+
+// console.log(addOne(5))
+// console.log(addTwo(5))
